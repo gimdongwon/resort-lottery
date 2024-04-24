aa4896254801507e797378a73dad7c072848011e
gimdongwon <ehddnjs8989@gmail.com>
2024-04-25 00:41:05 +0900

    complete app

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styles from "@/app/components/App.module.css";
 import Image from "next/image";
+import WinningNumber from "./WinningNumber";
 
 function App() {
   const [number, setNumber] = useState<number>(0);
@@ -20,35 +21,48 @@ function App() {
     setInputNumber(value);
   };
   return (
-    <main className={styles.main}>
-      <Image
-        src="https://fem.encar.com/assets/images/login/logo_ENCAR.svg"
-        width={100}
-        height={50}
-        alt="encar logo"
-      />
-      <h1 className={styles.title}>엔카 리조트 추첨</h1>
-      <div className={styles.wrap_input}>
-        <label htmlFor="maxNumber" className={styles.label}>
-          리조트 당첨 숫자 입력
-        </label>
-        <input
-          type="text"
-          id="maxNumber"
-          name="maxNumber"
-          className={styles.input_number}
-          min="1"
-          onChange={handleInputNumber}
-          placeholder="최대 숫자를 입력하세요"
+    <>
+      <main className={styles.main}>
+        <Image
+          src="https://fem.encar.com/assets/images/login/logo_ENCAR.svg"
+          width={100}
+          height={50}
+          alt="encar logo"
         />
-      </div>
-      {visible && <p className={styles.winning_number}>당첨번호 {number}</p>}
-      <div className={styles.area_btn}>
-        <button className={styles.btn_lottery} onClick={getRandomNumber}>
-          번호 추첨하기
-        </button>
-      </div>
-    </main>
+        <h1 className={styles.title}>엔카 리조트 추첨</h1>
+        <div className={styles.wrap_input}>
+          <label htmlFor="maxNumber" className={styles.label}>
+            리조트 당첨 숫자 입력
+          </label>
+          <input
+            type="text"
+            id="maxNumber"
+            name="maxNumber"
+            className={styles.input_number}
+            min="1"
+            onChange={handleInputNumber}
+            placeholder="최대 숫자를 입력하세요"
+          />
+        </div>
+        {visible && <WinningNumber winningNumber={number} />}
+        <div className={styles.area_btn}>
+          <button className={styles.btn_lottery} onClick={getRandomNumber}>
+            번호 추첨하기
+          </button>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <a href="https://github.com/gimdongwon" target="_blank">
+          made by winter1
+        </a>{" "}
+        ||{" "}
+        <a href="https://github.com/gimdongwon/resort-lottery" target="_blank">
+          source code
+        </a>
+        <br />
+        조작 방지를 위해 소스코드 첨부합니다
+      </footer>
+    </>
   );
 }
 
